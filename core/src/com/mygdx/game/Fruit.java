9c55999397958eeb5bd547316a0ede46f1cc4c39
DogukanPolatel <dogukanplatel1047@gmail.com>
2022-09-05 15:40:17 +0300

    forth commit

---
+++ core/src/com/mygdx/game/Fruit.java
@@ -0,0 +1,53 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+
+public class Fruit {
+
+    public static float radius = 60f;
+
+    public enum Type {
+        REGULAR, EXTRA, ENEMY, LIFE,GREENAPPLE
+    }
+
+    Type type;
+    Vector2 pos;
+    Vector2 velocity;
+
+    //pozisyon and hız
+    Fruit(Vector2 pos, Vector2 velocity) {
+        this.pos = pos;
+        this.velocity = velocity;
+        type = Type.REGULAR;
+
+
+    }
+//kullanıcı tıklanma yerine en yakın yer bulan fonk
+    public boolean clicked(Vector2 clicked){
+        if(pos.dst2(clicked)<=radius*radius+1){
+            return true;
+        }
+        return  false;
+    }
+
+    public final Vector2 getPos(){
+        return pos;
+    }
+
+    public boolean outofScreen(){
+
+        return(pos.y< -2f*radius);
+    }
+
+    public void update(float dt){
+
+        velocity.y -= dt*(Gdx.graphics.getHeight()*0.2f);
+        velocity.x -= dt*Math.signum(velocity.x)*3f;
+        pos.mulAdd(velocity,dt);
+
+
+
+    }
+
+}
--- core/src/com/mygdx/game/PlanetNinja.java
+++ core/src/com/mygdx/game/PlanetNinja.java
@@ -10,25 +10,50 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
 
+import java.util.Random;
+
 public class PlanetNinja extends ApplicationAdapter implements InputProcessor {
+
     SpriteBatch batch;
-    Texture background, apple, bamb, cherry, greenApple, banana, ananas, kavun, coconut, healBar;
+
+    //instance
+    Texture background, apple, bamb, cherry, greenApple, banana, coconut, healBar, heart;
+
     BitmapFont font;
+
     FreeTypeFontGenerator freeTypeFontGenerator;
-    int lives = 4;
-    int score = 5;
+
+    int lives = 0;
+    int score = 0;
     Sound sound;
+
     private double currentTime;
     private double gameOverTime = -1.0f;
+
     Music music;
+
+    Random random = new Random();
+    Array<Fruit> fruitArray = new Array<Fruit>();
+
+
+    float genCounter;
+    private final float genSpeedStart = 1.1f;
+    float genSpeed = genSpeedStart;
+
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+
+
+        //instance
+        heart = new Texture("heart.png");
         healBar = new Texture("healBar.png");
         background = new Texture("bg.png");
         apple = new Texture("apple1.png");
@@ -37,8 +62,10 @@ public class PlanetNinja extends ApplicationAdapter implements InputProcessor {
         greenApple = new Texture("greenApple.png");
         coconut = new Texture("Coconut.png");
         banana = new Texture("muz.png");
-        // ananas = new Texture("ananas.jpg");
-        //kavun = new Texture("kavun.jpg");
+
+
+        Fruit.radius = Math.max(Gdx.graphics.getHeight(), Gdx.graphics.getWidth()) / 17f;
+
 
         music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
 
@@ -48,7 +75,7 @@ public class PlanetNinja extends ApplicationAdapter implements InputProcessor {
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.color = Color.RED;
         parameter.size = 60;
-        parameter.characters = "0123456789  PCScrecutoplay .;: +-*#";
+        parameter.characters = "0123456789  LAYPCScrecutoplay .;: +-*#";
         font = freeTypeFontGenerator.generateFont(parameter);
 
     }
@@ -58,12 +85,13 @@ public class PlanetNinja extends ApplicationAdapter implements InputProcessor {
 
         batch.begin();
 
-        double newTime= TimeUtils.millis()/1000.0;//real time
-        System.out.println("newTime:"+newTime);
-        double frameTime=Math.min(newTime-currentTime,0.3);
-        float deltaTime=(float)frameTime;
-        System.out.println("detaTime"+deltaTime);
-        currentTime=newTime;
+        double newTime = TimeUtils.millis() / 1000.0;//real time
+        System.out.println("newTime:" + newTime);
+        double frameTime = Math.min(newTime - currentTime, 0.3);
+        float deltaTime = (float) frameTime;
+        System.out.println("detaTime" + deltaTime);
+        currentTime = newTime;
+        //konum -- background size
         batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         music.play();
         music.setVolume(10f);
@@ -71,24 +99,81 @@ public class PlanetNinja extends ApplicationAdapter implements InputProcessor {
 
 
         if (lives <= 0 && gameOverTime == 0f) {
-            //Oyun biiti
+            //game Over
 
             gameOverTime = currentTime;
         }
         if (lives > 0) {
-            //oyun modu
+            //game Mod
+            genSpeed -= deltaTime * 0.0008f;
+            if (genCounter <= 0f) {
+                genCounter = genSpeed * 1.6f;
+                addItem();
+            } else {
+                genCounter -= deltaTime;
+            }
+
 
             for (int i = 0; i < lives; i++) {
-//çizeceği texture,konum
+                //çizeceği texture,konum
                 batch.draw(bamb, i * 22f * 3, Gdx.graphics.getHeight() - 125f, 125f, 125f);
+            }
+
+            for (Fruit fruit : fruitArray) {
+                fruit.update(deltaTime);
+                switch (fruit.type) {
+                    case REGULAR:
+                        batch.draw(apple, fruit.getPos().x, fruit.getPos().y, Fruit.radius, Fruit.radius);
+                        break;
+                    case ENEMY:
+                        batch.draw(bamb, fruit.getPos().x, fruit.getPos().y, Fruit.radius * 1.8f, Fruit.radius * 1.8f);
+                        break;
+                    case EXTRA:
+                        batch.draw(cherry, fruit.getPos().x, fruit.getPos().y, Fruit.radius, Fruit.radius);
+                        break;
+                    case LIFE:
+                        batch.draw(heart, fruit.getPos().x, fruit.getPos().y, Fruit.radius * 0.9f, Fruit.radius * 0.9f);
+                        break;
+
+                    case GREENAPPLE:
+                        batch.draw(greenApple, fruit.getPos().x, fruit.getPos().y, Fruit.radius * 0.9f, Fruit.radius * 0.9f);
+
+                }
             }
 
 
         }
-        font.draw(batch, " #Score:", 20, 60);
+        font.draw(batch, " #Score:" + score, 20, 60);
+      /*  if(lives<=0){
+            font.draw(batch, "PLAY", Gdx.graphics.getWidth()*0.5f, Gdx.graphics.getHeight()*0.5f);
+        }*/
         //font.draw(batch, "Cut to Play", Gdx.graphics.getWidth() * 0.39f, Gdx.graphics.getHeight() * 0.5f);
         batch.end();
     }
+
+
+    private void addItem() {
+
+        float pos = random.nextFloat() * (Math.max(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+        //Konum ve Hız
+        Fruit item = new Fruit(new Vector2(pos, -Fruit.radius), new Vector2((Gdx.graphics.getWidth() * 0.3f) * (random.nextFloat() - 0.3f), (Gdx.graphics.getHeight() * 0.64f)));
+        float type = random.nextFloat();
+        if (type > 0.94) {
+            item.type = Fruit.Type.LIFE;
+        } else if (type > 0.8) {
+            item.type = Fruit.Type.EXTRA;
+        } else if (type > 0.6) {
+            item.type = Fruit.Type.ENEMY;
+        } else if (type > 0.5) {
+            item.type = Fruit.Type.GREENAPPLE;
+        }
+
+
+        fruitArray.add(item);
+
+
+    }
+
 
     @Override
     public void dispose() {
@@ -100,6 +185,7 @@ public class PlanetNinja extends ApplicationAdapter implements InputProcessor {
 
     }
 
+    //Input proCosser
     @Override
     public boolean keyDown(int keycode) {
         return false;
@@ -128,6 +214,64 @@ public class PlanetNinja extends ApplicationAdapter implements InputProcessor {
     //dokunduğunda ne olacak
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
+
+        if (lives <= 0 && currentTime - gameOverTime > 2f) {
+
+            //menu mod
+            gameOverTime = 0f;
+            score = 0;
+            lives = 4;
+            genSpeed = genSpeedStart;
+            fruitArray.clear();
+
+
+        } else {
+            int plusScore = 0;
+            Array<Fruit> toRemove = new Array<Fruit>();
+            Vector2 pos = new Vector2(screenX, Gdx.graphics.getHeight()-screenY);
+            for (Fruit f : fruitArray) {
+                System.out.println("getHeight - y: " + screenY);
+                System.out.println("getHeight - y: " + (Gdx.graphics.getHeight()-screenY));
+                System.out.println("getHeight - y: " + f.getPos());
+                System.out.println("distance: " + pos.dst2(f.pos));
+                System.out.println("distance: " + f.clicked(pos));
+                System.out.println("distance: " + Fruit.radius * Fruit.radius + 1);
+                if (f.clicked(pos)) {
+                    toRemove.add(f);
+
+
+                    switch (f.type) {
+                        case REGULAR:
+                            plusScore++;
+                            break;
+                        case ENEMY:
+                            lives--;
+                            break;
+                        case EXTRA:
+                            plusScore += 2;
+                            break;
+                        case LIFE:
+                            lives++;
+                            break;
+                        case GREENAPPLE:
+                            plusScore+=1;
+                            break;
+
+
+                    }
+                }
+            }
+
+            score += plusScore * plusScore;
+
+
+            for (Fruit f : toRemove) {
+                fruitArray.removeValue(f, true);
+            }
+
+        }
+
+
         return false;
     }
 
